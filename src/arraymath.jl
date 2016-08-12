ce42f74868d5c25f5236f0a699149f0920fe7a25
Andy Ferris <andy.ferris@roames.com.au>
2016-08-12 10:26:19 +1000

    Added more mutating rand! methods

--- src/arraymath.jl
+++ src/arraymath.jl
@@ -71,6 +71,7 @@ end
 # TODO allow ranges/collections as inputs...
 # Signatures = rand{SA <: StaticArray}(::AbstractRNG, range::AbstractArray, dims::Union{SA, Type{SA}})
 #              rand{SA <: StaticArray}(range::AbstractArray, dims::Union{SA, Type{SA}})
+# Also consider randcycle, randperm?
 @generated function Base.rand{SA <: StaticArray}(rng::AbstractRNG, ::Union{SA,Type{SA}})
     s = size(SA)
     T = eltype(SA)
@@ -107,5 +108,64 @@ end
     return quote
         $(Expr(:meta, :inline))
         $(Expr(:call, SA, Expr(:tuple, v...)))
+    end
+end
+
+# Why don't these two exist in Base?
+# @generated function Base.zeros!{SA <: StaticArray}(a::SA)
+# @generated function Base.ones!{SA <: StaticArray}(a::SA)
+
+@generated function Base.fill!{SA <: StaticArray}(a::SA, val)
+    l = length(SA)
+    T = eltype(SA)
+    exprs = [:(@inbounds a[$i] = valT) for i = 1:l]
+    return quote
+        $(Expr(:meta, :inline))
+        valT = convert($T, val)
+        $(Expr(:block, exprs...))
+        return a
+    end
+end
+
+@generated function Base.rand!{SA <: StaticArray}(rng::AbstractRNG, a::SA)
+    l = length(SA)
+    T = eltype(SA)
+    exprs = [:(@inbounds a[$i] = rand(rng, $T)) for i = 1:l]
+    return quote
+        $(Expr(:meta, :inline))
+        $(Expr(:block, exprs...))
+        return a
+    end
+end
+
+@generated function Base.rand!{N}(rng::MersenneTwister, a::StaticArray{Float64,N}) # ambiguity with AbstractRNG and non-Float64... possibly an optimized form in Base?
+    l = length(a)
+    exprs = [:(@inbounds a[$i] = rand(rng, Float64)) for i = 1:l]
+    return quote
+        $(Expr(:meta, :inline))
+        $(Expr(:block, exprs...))
+        return a
+    end
+end
+
+@generated function Base.randn!{SA <: StaticArray}(rng::AbstractRNG, a::SA)
+    l = length(SA)
+    T = eltype(SA)
+    exprs = [:(@inbounds a[$i] = randn(rng, $T)) for i = 1:l]
+    return quote
+        $(Expr(:meta, :inline))
+        $(Expr(:block, exprs...))
+        return a
+    end
+end
+
+@generated function Base.randexp!{SA <: StaticArray}(rng::AbstractRNG, a::SA)
+    l = length(SA)
+    T = eltype(SA)
+    exprs = [:(@inbounds a[$i] = randexp(rng, $T)) for i = 1:l]
+    return quote
+        $(Expr(:meta, :inline))
+        $(Expr(:block, exprs...))
+        return a
     end
 end
