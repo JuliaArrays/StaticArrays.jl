f90e612229bdd9b38c5e9c3a4d896cf7b501e791
benjamin shall <benji.l.shall@gmail.com>
2017-05-29 18:14:09 +0200

    Move tests from arraymath.jl to broadcast.jl and add more tests

--- test/arraymath.jl
+++ test/arraymath.jl
@@ -1,36 +1,4 @@
 @testset "Array math" begin
-    @testset "AbstractArray-of-StaticArray with scalar math" begin
-        v = SVector{2,Float64}[SVector{2,Float64}(1,1)]
-        @test @inferred(v .* 1.0)::typeof(v) == v
-        @test @inferred(1 .- v)::typeof(v) == v .- v
-        v2 = SVector{2,Int}[SVector{2,Int}(1,1)]
-        @test @inferred(v2 .* 1.0)::typeof(v) == v
-    end
-
-    @testset "Array-scalar math" begin
-        m = @SMatrix [1 2; 3 4]
-
-        @test @inferred(m .+ 1) === @SMatrix [2 3; 4 5]
-        @test @inferred(1 .+ m) === @SMatrix [2 3; 4 5]
-        @test @inferred(m .* 2) === @SMatrix [2 4; 6 8]
-        @test @inferred(2 .* m) === @SMatrix [2 4; 6 8]
-        @test @inferred(m .- 1) === @SMatrix [0 1; 2 3]
-        @test @inferred(1 .- m) === @SMatrix [0 -1; -2 -3]
-        @test @inferred(m ./ 2) === @SMatrix [0.5 1.0; 1.5 2.0]
-        @test @inferred(12 ./ m) === @SMatrix [12.0 6.0; 4.0 3.0]
-
-    end
-
-    @testset "Elementwise array math" begin
-        m1 = @SMatrix [1 2; 3 4]
-        m2 = @SMatrix [4 3; 2 1]
-
-        @test @inferred(m1 .+ m2) === @SMatrix [5 5; 5 5]
-        @test @inferred(m1 .* m2) === @SMatrix [4 6; 6 4]
-        @test @inferred(m1 .- m2) === @SMatrix [-3 -1; 1 3]
-        @test @inferred(m1 ./ m2) === @SMatrix [0.25 2/3; 1.5 4.0]
-    end
-
     @testset "zeros() and ones()" begin
         @test @inferred(zeros(SVector{3,Float64})) === @SVector [0.0, 0.0, 0.0]
         @test @inferred(zeros(SVector{3,Int})) === @SVector [0, 0, 0]
