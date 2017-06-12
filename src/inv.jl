abe95f42ecb60a0298f091f1d782c1c7f45e0086
Chris Foster <chris42f@gmail.com>
2017-06-12 23:23:29 +1000

    Specialized 4x4 matrix inversion based on partitioning
    This version is fast and works well for well conditioned matrices.  It
    seems somewhat better numerically conditioned than the adjugate
    expansion for poorly conditioned matrices, but can still fail quite
    badly in some cases.

--- src/inv.jl
+++ src/inv.jl
@@ -30,6 +30,34 @@ end
     @inbounds return newtype((y0[1], y1[1], y2[1], y0[2], y1[2], y2[2], y0[3], y1[3], y2[3]))
 end
 
+Base.@pure function splitrange(r::SUnitRange)
+    mid = (first(r) + last(r)) ÷ 2
+    (SUnitRange(first(r), mid), SUnitRange(mid+1, last(r)))
+end
+
+@noinline function _inv(::Size{(4,4)}, A)
+    # Partition matrix into four 2x2 blocks.  For 4x4 matrices this seems to be
+    # more stable than directly using the adjugate expansion.
+    # See http://www.freevec.org/function/inverse_matrix_4x4_using_partitioning
+    #
+    # TODO: This decomposition works in higher dimensions, but numerical
+    # stability doesn't seem good for badly conditioned matrices.  Can be
+    # fixed?
+    (i1,i2) = splitrange(SUnitRange(1,4))
+    @inbounds P = A[i1,i1]
+    @inbounds Q = A[i1,i2]
+    @inbounds R = A[i2,i1]
+    @inbounds S = A[i2,i2]
+    invP = inv(P)
+    invP_Q = invP*Q
+    S2 = inv(S - R*invP_Q)
+    R2 = -S2*(R*invP)
+    Q2 = -invP_Q*S2
+    P2 = invP - invP_Q*R2
+    [[P2 Q2];
+     [R2 S2]]
+end
+
 @inline function _inv(::Size, A)
     T = eltype(A)
     S = typeof((one(T)*zero(T) + zero(T))/one(T))
