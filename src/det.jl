40020140c9a003fe2c45d2c29967199855bc1d64
Tsur Herman <tsur.herman@gmail.com>
2017-08-01 18:36:09 +0300

    simplify det.jl covers all cases , matches Base

--- src/det.jl
+++ src/det.jl
@@ -1,45 +1,26 @@
-@inline det(A::StaticMatrix) = _det(Size(A), A)
-@inline logdet(A::StaticMatrix) = _logdet(Size(A), A)
+@inline function det(A::StaticMatrix)
+    T = eltype(A)
+    S = typeof((one(T)*zero(T) + zero(T))/one(T))
+    _det(Size(A),A,S)
+end
 
-@inline _det(::Size{(1,1)}, A::StaticMatrix) = @inbounds return A[1]
+@inline logdet(A::StaticMatrix) = log(det(A))
 
-@inline function _det(::Size{(2,2)}, A::StaticMatrix)
+@inline _det(::Size{(1,1)}, A::StaticMatrix,::Type) = @inbounds return A[1]
+
+@inline function _det(::Size{(2,2)}, A::StaticMatrix, S::Type)
+    A = similar_type(A,S)(A)
     @inbounds return A[1]*A[4] - A[3]*A[2]
 end
 
-@inline function _det(::Size{(3,3)}, A::StaticMatrix)
+@inline function _det(::Size{(3,3)}, A::StaticMatrix, S::Type)
+    A = similar_type(A,S)(A)
     @inbounds x0 = SVector(A[1], A[2], A[3])
     @inbounds x1 = SVector(A[4], A[5], A[6])
     @inbounds x2 = SVector(A[7], A[8], A[9])
     return vecdot(x0, cross(x1, x2))
 end
 
-@generated function det(A::StaticMatrix{<:Any,<:Any,T}) where T <:Unsigned
-    T1 = promote_type(T,typeof(signed(one(T))))
-    (T1 <: Unsigned) && (T1 = Float64) #if still unsigned cast to Float64
-    return quote
-        det(similar_type(A,$T1)(A))
-    end
-end
-
-@inline _logdet(S::Union{Size{(1,1)},Size{(2,2)},Size{(3,3)},Size{(4,4)}}, A::StaticMatrix) = log(_det(S, A))
-
-for (symb, f) in [(:_det, :det), (:_logdet, :logdet)]
-    eval(quote
-        @generated function $symb{S}(::Size{S}, A::StaticMatrix)
-            if S[1] != S[2]
-                throw(DimensionMismatch("matrix is not square"))
-            end
-            return quote # Implementation from Base
-                @_inline_meta
-                T = eltype(A)
-                T2 = typeof((one(T)*zero(T) + zero(T))/one(T))
-                if istriu(A) || istril(A)
-                    return convert(T2, $($f)(UpperTriangular(A))) # Is this a Julia bug that a convert is not type stable??
-                end
-                AA = convert(Array{T2}, A)
-                return $($f)(lufact(AA))
-            end
-        end
-    end)
+@inline function _det(::Size, A::StaticMatrix,::Type)
+    return det(Matrix(A))
 end
