0cd325ac197dde9c521dff613281b8ddafcf36cf
Tsur Herman <tsur.herman@gmail.com>
2017-08-05 09:55:47 +0300

    Make inv similar to det and clear noise

--- src/det.jl
+++ src/det.jl
@@ -21,6 +21,23 @@ end
     return vecdot(x0, cross(x1, x2))
 end
 
+@inline function _det(::Size{(4,4)}, A::StaticMatrix,S::Type)
+    A = similar_type(A,S)(A)
+    @inbounds return (
+        A[13] * A[10]  * A[7]  * A[4]  - A[9] * A[14] * A[7]  * A[4]   -
+        A[13] * A[6]   * A[11] * A[4]  + A[5] * A[14] * A[11] * A[4]   +
+        A[9]  * A[6]   * A[15] * A[4]  - A[5] * A[10] * A[15] * A[4]   -
+        A[13] * A[10]  * A[3]  * A[8]  + A[9] * A[14] * A[3]  * A[8]   +
+        A[13] * A[2]   * A[11] * A[8]  - A[1] * A[14] * A[11] * A[8]   -
+        A[9]  * A[2]   * A[15] * A[8]  + A[1] * A[10] * A[15] * A[8]   +
+        A[13] * A[6]   * A[3]  * A[12] - A[5] * A[14] * A[3]  * A[12]  -
+        A[13] * A[2]   * A[7]  * A[12] + A[1] * A[14] * A[7]  * A[12]  +
+        A[5]  * A[2]   * A[15] * A[12] - A[1] * A[6]  * A[15] * A[12]  -
+        A[9]  * A[6]   * A[3]  * A[16] + A[5] * A[10] * A[3]  * A[16]  +
+        A[9]  * A[2]   * A[7]  * A[16] - A[1] * A[10] * A[7]  * A[16]  -
+        A[5]  * A[2]   * A[11] * A[16] + A[1] * A[6]  * A[11] * A[16])
+end
+
 @inline function _det(::Size, A::StaticMatrix,::Type)
     return det(Matrix(A))
 end
--- src/inv.jl
+++ src/inv.jl
@@ -1,19 +1,18 @@
-@inline inv(A::StaticMatrix) = _inv(Size(A), A)
-
-@inline _inv(::Size{(1,1)}, A) = similar_type(typeof(A), typeof(inv(one(eltype(A)))))(inv(A[1]))
-
-@inline function _inv(::Size{(2,2)}, A)
+@inline function inv(A::StaticMatrix)
     T = eltype(A)
     S = typeof((one(T)*zero(T) + zero(T))/one(T))
-    newtype = similar_type(A, S)
+    _inv(Size(A),A,S)
+end
 
+@inline _inv(::Size{(1,1)}, A, S::Type) = inv(A[1])
+
+@inline function _inv(::Size{(2,2)}, A, S::Type)
+    newtype = similar_type(A, S)
     d = det(A)
     @inbounds return newtype((A[4]/d, -(A[2]/d), -(A[3]/d), A[1]/d))
 end
 
-@inline function _inv(::Size{(3,3)}, A)
-    T = eltype(A)
-    S = typeof((one(T)*zero(T) + zero(T))/one(T))
+@inline function _inv(::Size{(3,3)}, A,S::Type)
     newtype = similar_type(A, S)
 
     @inbounds x0 = SVector{3,S}(A[1], A[2], A[3])
@@ -30,7 +29,8 @@ end
     @inbounds return newtype((y0[1], y1[1], y2[1], y0[2], y1[2], y2[2], y0[3], y1[3], y2[3]))
 end
 
-@inline function _inv(::Size{(4,4)}, A)
+@inline function _inv(::Size{(4,4)}, A, S::Type)
+    A = similar_type(A,S)(A)
     idet = 1/det(A)
     B =  @SMatrix [
         (A[2,3]*A[3,4]*A[4,2] - A[2,4]*A[3,3]*A[4,2] + A[2,4]*A[3,2]*A[4,3] - A[2,2]*A[3,4]*A[4,3] - A[2,3]*A[3,2]*A[4,4] + A[2,2]*A[3,3]*A[4,4]) * idet
@@ -52,23 +52,9 @@ end
         (A[1,3]*A[2,4]*A[3,1] - A[1,4]*A[2,3]*A[3,1] + A[1,4]*A[2,1]*A[3,3] - A[1,1]*A[2,4]*A[3,3] - A[1,3]*A[2,1]*A[3,4] + A[1,1]*A[2,3]*A[3,4]) * idet
         (A[1,4]*A[2,2]*A[3,1] - A[1,2]*A[2,4]*A[3,1] - A[1,4]*A[2,1]*A[3,2] + A[1,1]*A[2,4]*A[3,2] + A[1,2]*A[2,1]*A[3,4] - A[1,1]*A[2,2]*A[3,4]) * idet
         (A[1,2]*A[2,3]*A[3,1] - A[1,3]*A[2,2]*A[3,1] + A[1,3]*A[2,1]*A[3,2] - A[1,1]*A[2,3]*A[3,2] - A[1,2]*A[2,1]*A[3,3] + A[1,1]*A[2,2]*A[3,3]) * idet]
-        return SMatrix{4,4,eltype(B),16}(B)
+        return A = similar_type(A,S)(B)
 end
 
-@inline function _inv(::Size, A)
-    T = eltype(A)
-    S = typeof((one(T)*zero(T) + zero(T))/one(T))
-    AA = convert(Array{S}, A) # lufact() doesn't work with StaticArrays at the moment... and the branches below must be type-stable
-    if istriu(A)
-        Ai_ut = inv(UpperTriangular(AA))
-        # TODO double check these routines leave the parent in a clean (upper triangular) state
-        return Size(A)(parent(Ai_ut)) # Return a `SizedArray`
-    elseif istril(A)
-        Ai_lt = inv(LowerTriangular(AA))
-        # TODO double check these routines leave the parent in a clean (lower triangular) state
-        return Size(A)(parent(Ai_lt)) # Return a `SizedArray`
-    else
-        Ai_lu = inv(lufact(AA))
-        return Size(A)(Ai_lu) # Return a `SizedArray`
-    end
+@inline function _inv(::Size, A ,::Type)
+    inv(Matrix(A))
 end
