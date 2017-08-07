e578b9b929446ea4ed42f4d58e4fe74de2e6ad25
Tsur Herman <tsur.herman@gmail.com>
2017-08-07 09:40:37 +0300

    Avoid variable name changing type, moved promotion to dispatching function

--- src/det.jl
+++ src/det.jl
@@ -1,28 +1,26 @@
 @inline function det(A::StaticMatrix)
     T = eltype(A)
     S = typeof((one(T)*zero(T) + zero(T))/one(T))
-    _det(Size(A),A,S)
+    A_S = similar_type(A,S)(A)
+    _det(Size(A_S),A_S)
 end
 
 @inline logdet(A::StaticMatrix) = log(det(A))
 
-@inline _det(::Size{(1,1)}, A::StaticMatrix,S::Type) = @inbounds return convert(S,A[1])
+@inline _det(::Size{(1,1)}, A::StaticMatrix) = @inbounds return A[1]
 
-@inline function _det(::Size{(2,2)}, A::StaticMatrix, S::Type)
-    A = similar_type(A,S)(A)
+@inline function _det(::Size{(2,2)}, A::StaticMatrix)
     @inbounds return A[1]*A[4] - A[3]*A[2]
 end
 
-@inline function _det(::Size{(3,3)}, A::StaticMatrix, S::Type)
-    A = similar_type(A,S)(A)
+@inline function _det(::Size{(3,3)}, A::StaticMatrix)
     @inbounds x0 = SVector(A[1], A[2], A[3])
     @inbounds x1 = SVector(A[4], A[5], A[6])
     @inbounds x2 = SVector(A[7], A[8], A[9])
     return vecdot(x0, cross(x1, x2))
 end
 
-@inline function _det(::Size{(4,4)}, A::StaticMatrix,S::Type)
-    A = similar_type(A,S)(A)
+@inline function _det(::Size{(4,4)}, A::StaticMatrix)
     @inbounds return (
         A[13] * A[10]  * A[7]  * A[4]  - A[9] * A[14] * A[7]  * A[4]   -
         A[13] * A[6]   * A[11] * A[4]  + A[5] * A[14] * A[11] * A[4]   +
@@ -38,6 +36,6 @@ end
         A[5]  * A[2]   * A[11] * A[16] + A[1] * A[6]  * A[11] * A[16])
 end
 
-@inline function _det(::Size, A::StaticMatrix,::Type)
+@inline function _det(::Size, A::StaticMatrix)
     return det(Matrix(A))
 end
--- src/inv.jl
+++ src/inv.jl
@@ -1,24 +1,24 @@
 @inline function inv(A::StaticMatrix)
     T = eltype(A)
     S = typeof((one(T)*zero(T) + zero(T))/one(T))
-    _inv(Size(A),A,S)
+    A_S = similar_type(A,S)(A)
+    _inv(Size(A_S),A_S)
 end
 
-@inline _inv(::Size{(1,1)}, A, S::Type) = similar_type(A,S)(inv(A[1]))
+@inline _inv(::Size{(1,1)}, A) = similar_type(A)(inv(A[1]))
 
-@inline function _inv(::Size{(2,2)}, A, S::Type)
-    newtype = similar_type(A,S)
-    A = newtype(A)
+@inline function _inv(::Size{(2,2)}, A)
+    newtype = similar_type(A)
     idet = 1/det(A)
     @inbounds return newtype((A[4]*idet, -(A[2]*idet), -(A[3]*idet), A[1]*idet))
 end
 
-@inline function _inv(::Size{(3,3)}, A,S::Type)
-    newtype = similar_type(A, S)
+@inline function _inv(::Size{(3,3)}, A)
+    newtype = similar_type(A)
 
-    @inbounds x0 = SVector{3,S}(A[1], A[2], A[3])
-    @inbounds x1 = SVector{3,S}(A[4], A[5], A[6])
-    @inbounds x2 = SVector{3,S}(A[7], A[8], A[9])
+    @inbounds x0 = SVector{3}(A[1], A[2], A[3])
+    @inbounds x1 = SVector{3}(A[4], A[5], A[6])
+    @inbounds x2 = SVector{3}(A[7], A[8], A[9])
 
     y0 = cross(x1,x2)
     d  = vecdot(x0, y0)
@@ -30,8 +30,7 @@ end
     @inbounds return newtype((y0[1], y1[1], y2[1], y0[2], y1[2], y2[2], y0[3], y1[3], y2[3]))
 end
 
-@inline function _inv(::Size{(4,4)}, A, S::Type)
-    A = similar_type(A,S)(A)
+@inline function _inv(::Size{(4,4)}, A)
     idet = 1/det(A)
     B =  @SMatrix [
         (A[2,3]*A[3,4]*A[4,2] - A[2,4]*A[3,3]*A[4,2] + A[2,4]*A[3,2]*A[4,3] - A[2,2]*A[3,4]*A[4,3] - A[2,3]*A[3,2]*A[4,4] + A[2,2]*A[3,3]*A[4,4]) * idet
@@ -53,9 +52,9 @@ end
         (A[1,3]*A[2,4]*A[3,1] - A[1,4]*A[2,3]*A[3,1] + A[1,4]*A[2,1]*A[3,3] - A[1,1]*A[2,4]*A[3,3] - A[1,3]*A[2,1]*A[3,4] + A[1,1]*A[2,3]*A[3,4]) * idet
         (A[1,4]*A[2,2]*A[3,1] - A[1,2]*A[2,4]*A[3,1] - A[1,4]*A[2,1]*A[3,2] + A[1,1]*A[2,4]*A[3,2] + A[1,2]*A[2,1]*A[3,4] - A[1,1]*A[2,2]*A[3,4]) * idet
         (A[1,2]*A[2,3]*A[3,1] - A[1,3]*A[2,2]*A[3,1] + A[1,3]*A[2,1]*A[3,2] - A[1,1]*A[2,3]*A[3,2] - A[1,2]*A[2,1]*A[3,3] + A[1,1]*A[2,2]*A[3,3]) * idet]
-        return A = similar_type(A,S)(B)
+        return similar_type(A)(B)
 end
 
-@inline function _inv(::Size, A , S::Type)
-    similar_type(A,S)(inv(Matrix(A)))
+@inline function _inv(::Size, A)
+    similar_type(A)(inv(Matrix(A)))
 end
