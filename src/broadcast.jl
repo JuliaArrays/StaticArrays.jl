a2995d56037292320284bf9662abbb8738b85b5b
Pablo San-Jose <lekand@gmail.com>
2017-12-07 18:40:04 +0100

    patched broadcast!, added version check

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -2,34 +2,74 @@
 ## broadcast! ##
 ################
 
-import Base.Broadcast:
+if VERSION < v"0.7.0-DEV.2638"   
+    ## Old Broadcast API ##
+    import Base.Broadcast:
+    _containertype, promote_containertype, broadcast_indices,
+    broadcast_c, broadcast_c!
+
+    # Add StaticArray as a new output type in Base.Broadcast promotion machinery.
+    # This isn't the precise output type, just a placeholder to return from
+    # promote_containertype, which will control dispatch to our broadcast_c.
+    _containertype(::Type{<:StaticArray}) = StaticArray
+    _containertype(::Type{<:RowVector{<:Any,<:SVector}}) = StaticArray
+
+    # With the above, the default promote_containertype gives reasonable defaults:
+    #   StaticArray, StaticArray -> StaticArray
+    #   Array, StaticArray       -> Array
+    #
+    # We could be more precise about the latter, but this isn't really possible
+    # without using Array{N} rather than Array in Base's promote_containertype.
+    #
+    # Base also has broadcast with tuple + Array, but while implementing this would
+    # be consistent with Base, it's not exactly clear it's a good idea when you can
+    # just use an SVector instead?
+    promote_containertype(::Type{StaticArray}, ::Type{Any}) = StaticArray
+    promote_containertype(::Type{Any}, ::Type{StaticArray}) = StaticArray
+
+    # Override for when output type is deduced to be a StaticArray.
+    @inline function broadcast_c(f, ::Type{StaticArray}, as...)
+        _broadcast(f, broadcast_sizes(as...), as...)
+    end
+
+    # TODO: This signature could be relaxed to (::Any, ::Type{StaticArray}, ::Type, ...), though
+    # we'd need to rework how _broadcast!() and broadcast_sizes() interact with normal AbstractArray.
+    @inline function broadcast_c!(f, ::Type{StaticArray}, ::Type{StaticArray}, dest, as...)
+        _broadcast!(f, Size(dest), dest, broadcast_sizes(as...), as...)
+    end
+else   
+    ## New Broadcast API ##
+    import Base.Broadcast:
     BroadcastStyle, AbstractArrayStyle, broadcast
 
-# Add a new BroadcastStyle for StaticArrays, derived from AbstractArrayStyle 
-# A constructor that changes the style parameter N (array dimension) is also required
-struct StaticArrayStyle{N} <: AbstractArrayStyle{N} end
-StaticArrayStyle{M}(::Val{N}) where {M,N} = StaticArrayStyle{N}()
+    # Add a new BroadcastStyle for StaticArrays, derived from AbstractArrayStyle 
+    # A constructor that changes the style parameter N (array dimension) is also required
+    struct StaticArrayStyle{N} <: AbstractArrayStyle{N} end
+    StaticArrayStyle{M}(::Val{N}) where {M,N} = StaticArrayStyle{N}()
 
-BroadcastStyle(::Type{<:StaticArray{D, T, N}}) where {D, T, N} = StaticArrayStyle{N}()
+    BroadcastStyle(::Type{<:StaticArray{D, T, N}}) where {D, T, N} = StaticArrayStyle{N}()
 
-broadcast_indices(A::StaticArray) = indices(A)
+    # Add a broadcast method that calls the old @generated routine
+    @inline function broadcast(f, ::StaticArrayStyle{N}, ::Type{ElType}, inds::Tuple{Vararg{AbstractUnitRange, M}} where M, As...) where {N, ElType} 
+        _broadcast(f, broadcast_sizes(As...), As...)
+    end
 
-# Add a broadcast method that calls the old @generated broadcast
-@inline function broadcast(f, ::StaticArrayStyle{N}, ::Type{ElType}, inds::Tuple{Vararg{AbstractUnitRange, M}} where M, As...) where {N, ElType} 
-    _broadcast(f, broadcast_sizes(As...), As...)
+    # Add a specialized broadcast! method that overrides the Base fallback and calls the old routine
+    # TODO: This signature could be relaxed to (::AbstractArray, ::Vararg{StaticArray,N}), ::Type, ...), though
+    # we'd need to rework how _broadcast!() and broadcast_sizes() interact with normal AbstractArray.
+    @inline function broadcast!(f, C::StaticArray, As::Vararg{StaticArray,N}) where {N}
+        _broadcast!(f, Size(C), C, broadcast_sizes(As...), As...)
+    end
 end
 
-# # TODO: This signature could be relaxed to (::Any, ::Type{StaticArray}, ::Type, ...), though
-# # we'd need to rework how _broadcast!() and broadcast_sizes() interact with normal AbstractArray.
-@inline function broadcast_c!(f, ::Type{StaticArray}, ::Type{StaticArray}, dest, as...)
-    _broadcast!(f, Size(dest), dest, broadcast_sizes(as...), as...)
-end
 
 # _broadcast!(f, ::Size{newsize}, dest::StaticArray, s::Tuple{Vararg{Size}}, as...) where {newsize}
 
 ##############################################
 ## Old broadcast machinery for StaticArrays ##
 ##############################################
+
+broadcast_indices(A::StaticArray) = indices(A)
  
 @inline broadcast_sizes(a::RowVector{<:Any,<:SVector}, as...) = (Size(a), broadcast_sizes(as...)...)
 @inline broadcast_sizes(a::StaticArray, as...) = (Size(a), broadcast_sizes(as...)...)
@@ -117,57 +157,57 @@ if VERSION < v"0.7.0-DEV"
     end
 end
 
-# ################
-# ## broadcast! ##
-# ################
+###############################################
+## Old broadcast! machinery for StaticArrays ##
+###############################################
 
-# @generated function _broadcast!(f, ::Size{newsize}, dest::StaticArray, s::Tuple{Vararg{Size}}, as...) where {newsize}
-#     sizes = [sz.parameters[1] for sz ∈ s.parameters]
-#     sizes = tuple(sizes...)
+@generated function _broadcast!(f, ::Size{newsize}, dest::StaticArray, s::Tuple{Vararg{Size}}, as...) where {newsize}
+    sizes = [sz.parameters[1] for sz ∈ s.parameters]
+    sizes = tuple(sizes...)
 
-#     ndims = 0
-#     for i = 1:length(sizes)
-#         ndims = max(ndims, length(sizes[i]))
-#     end
+    ndims = 0
+    for i = 1:length(sizes)
+        ndims = max(ndims, length(sizes[i]))
+    end
 
-#     for i = 1:length(sizes)
-#         s = sizes[i]
-#         for j = 1:length(s)
-#             if s[j] != 1 && s[j] != (j <= length(newsize) ? newsize[j] : 1)
-#                 throw(DimensionMismatch("Tried to broadcast to destination sized $newsize from inputs sized $sizes"))
-#             end
-#         end
-#     end
+    for i = 1:length(sizes)
+        s = sizes[i]
+        for j = 1:length(s)
+            if s[j] != 1 && s[j] != (j <= length(newsize) ? newsize[j] : 1)
+                throw(DimensionMismatch("Tried to broadcast to destination sized $newsize from inputs sized $sizes"))
+            end
+        end
+    end
 
-#     exprs = Array{Expr}(newsize)
-#     j = 1
-#     more = prod(newsize) > 0
-#     current_ind = ones(Int, max(length(newsize), length.(sizes)...))
-#     while more
-#         exprs_vals = [(!(as[i] <: AbstractArray) ? :(as[$i]) : :(as[$i][$(broadcasted_index(sizes[i], current_ind))])) for i = 1:length(sizes)]
-#         exprs[current_ind...] = :(dest[$j] = f($(exprs_vals...)))
+    exprs = Array{Expr}(newsize)
+    j = 1
+    more = prod(newsize) > 0
+    current_ind = ones(Int, max(length(newsize), length.(sizes)...))
+    while more
+        exprs_vals = [(!(as[i] <: AbstractArray) ? :(as[$i]) : :(as[$i][$(broadcasted_index(sizes[i], current_ind))])) for i = 1:length(sizes)]
+        exprs[current_ind...] = :(dest[$j] = f($(exprs_vals...)))
 
-#         # increment current_ind (maybe use CartesianRange?)
-#         current_ind[1] += 1
-#         for i ∈ 1:length(newsize)
-#             if current_ind[i] > newsize[i]
-#                 if i == length(newsize)
-#                     more = false
-#                     break
-#                 else
-#                     current_ind[i] = 1
-#                     current_ind[i+1] += 1
-#                 end
-#             else
-#                 break
-#             end
-#         end
-#         j += 1
-#     end
+        # increment current_ind (maybe use CartesianRange?)
+        current_ind[1] += 1
+        for i ∈ 1:length(newsize)
+            if current_ind[i] > newsize[i]
+                if i == length(newsize)
+                    more = false
+                    break
+                else
+                    current_ind[i] = 1
+                    current_ind[i+1] += 1
+                end
+            else
+                break
+            end
+        end
+        j += 1
+    end
 
-#     return quote
-#         @_inline_meta
-#         @inbounds $(Expr(:block, exprs...))
-#         return dest
-#     end
-# end
+    return quote
+        @_inline_meta
+        @inbounds $(Expr(:block, exprs...))
+        return dest
+    end
+end
