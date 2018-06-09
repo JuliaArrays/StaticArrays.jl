9cad568d909d21b39a6c9b01bd4e5edcb46961dd
Twan Koolen <koolen.twan@gmail.com>
2018-06-09 12:27:47 -0400

    De-nest LU test sets so that there's more feedback on progress.

--- test/lu.jl
+++ test/lu.jl
@@ -1,41 +1,39 @@
 using StaticArrays, Compat.Test
 
-@testset "LU decomposition (pivot=$pivot)" for pivot in (true, false)
-    @testset "$m×$n" for m in [0:4..., 15], n in [0:4..., 15]
-        a = SMatrix{m,n,Int}(1:(m*n))
-        l, u, p = @inferred(lu(a, Val{pivot}))
+@testset "LU decomposition ($m×$n, pivot=$pivot)" for pivot in (true, false), m in [0:4..., 15], n in [0:4..., 15]
+    a = SMatrix{m,n,Int}(1:(m*n))
+    l, u, p = @inferred(lu(a, Val{pivot}))
 
-        # expected types
-        @test p isa SVector{m,Int}
-        if m==n
-            @test l isa LowerTriangular{<:Any,<:SMatrix{m,n}}
-            @test u isa UpperTriangular{<:Any,<:SMatrix{m,n}}
-        else
-            @test l isa SMatrix{m,min(m,n)}
-            @test u isa SMatrix{min(m,n),n}
-        end
-
-        if pivot
-            # p is a permutation
-            @test sort(p) == collect(1:m)
-        else
-            @test p == collect(1:m)
-        end
-
-        # l is unit lower triangular
-        for i=1:m, j=(i+1):size(l,2)
-            @test iszero(l[i,j])
-        end
-        for i=1:size(l,2)
-            @test l[i,i] == 1
-        end
-
-        # u is upper triangular
-        for i=1:size(u,1), j=1:i-1
-            @test iszero(u[i,j])
-        end
-
-        # decomposition is correct
-        @test l*u ≈ a[p,:]
+    # expected types
+    @test p isa SVector{m,Int}
+    if m==n
+        @test l isa LowerTriangular{<:Any,<:SMatrix{m,n}}
+        @test u isa UpperTriangular{<:Any,<:SMatrix{m,n}}
+    else
+        @test l isa SMatrix{m,min(m,n)}
+        @test u isa SMatrix{min(m,n),n}
     end
+
+    if pivot
+        # p is a permutation
+        @test sort(p) == collect(1:m)
+    else
+        @test p == collect(1:m)
+    end
+
+    # l is unit lower triangular
+    for i=1:m, j=(i+1):size(l,2)
+        @test iszero(l[i,j])
+    end
+    for i=1:size(l,2)
+        @test l[i,i] == 1
+    end
+
+    # u is upper triangular
+    for i=1:size(u,1), j=1:i-1
+        @test iszero(u[i,j])
+    end
+
+    # decomposition is correct
+    @test l*u ≈ a[p,:]
 end
