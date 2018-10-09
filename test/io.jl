d98e49bd5a59f1d0bf4c9dd7ab3fd2de8ba7fc86
Chris Foster <chris42f@gmail.com>
2018-10-09 18:26:11 +1000

    Reinstate read(::IO,::Type{<:StaticArray}) (#515)
    This was removed in the rush to 0.7 compatibility but makes sense for immutable `StaticArray` types.

--- test/io.jl
+++ test/io.jl
@@ -13,10 +13,10 @@ end
 @testset "Binary IO" begin
     @testset "read!" begin
         # Read static arrays from a stream which was serialized elementwise
-        @test read!(write_buf(UInt8, 1,2,3), SVector{3,UInt8})         === SVector{3,UInt8}(1,2,3)
-        @test read!(write_buf(Int32, -1,2,3), SVector{3,Int32})        === SVector{3,Int32}(-1,2,3)
-        @test read!(write_buf(Float64, 1,2,3), SVector{3,Float64})     === SVector{3,Float64}(1,2,3)
-        @test read!(write_buf(Float64, 1,2,3,4), SMatrix{2,2,Float64}) === @SMatrix [1.0 3.0; 2.0 4.0]
+        @test read(write_buf(UInt8, 1,2,3), SVector{3,UInt8})         === SVector{3,UInt8}(1,2,3)
+        @test read(write_buf(Int32, -1,2,3), SVector{3,Int32})        === SVector{3,Int32}(-1,2,3)
+        @test read(write_buf(Float64, 1,2,3), SVector{3,Float64})     === SVector{3,Float64}(1,2,3)
+        @test read(write_buf(Float64, 1,2,3,4), SMatrix{2,2,Float64}) === @SMatrix [1.0 3.0; 2.0 4.0]
     end
 
     @testset "write" begin
@@ -33,6 +33,10 @@ end
         @test read!(write_buf(Int32, -1,2,3),    zeros(MVector{3,Int32}))     == MVector{3,Int32}(-1,2,3)
         @test read!(write_buf(Float64, 1,2,3),   zeros(MVector{3,Float64}))   == MVector{3,Float64}(1,2,3)
         @test read!(write_buf(Float64, 1,2,3,4), zeros(MMatrix{2,2,Float64})) == @MMatrix [1.0 3.0; 2.0 4.0]
+        # Test that read! does, in fact, modify an MVector rather than return a copy.
+        m = zeros(MVector{3,UInt8})
+        read!(write_buf(UInt8, 1,2,3), m)
+        @test m == MVector{3,UInt8}(1,2,3)
     end
 end
 
