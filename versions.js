a5056a2c898e0a2bb2d672e78bbd1161b4533585
autodocs <autodocs>
2018-11-17 17:53:10 +0000

    build based on cf87ee2

--- versions.js
+++ versions.js
@@ -1,10 +1,12 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.11",
   "release-0.10",
   "release-0.9",
   "release-0.7",
   "release-0.6",
+  "v0.11.0",
   "v0.10.0",
   "v0.9.2",
   "v0.9.1",
