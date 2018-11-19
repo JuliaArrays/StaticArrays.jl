abaf4834342950c65e0d16032d5f3326f23164bb
Simon Christ <SimonChrist@gmx.de>
2018-11-19 12:53:26 +0100

    No tuple creation for SizedArrays

--- src/abstractarray.jl
+++ src/abstractarray.jl
@@ -154,6 +154,7 @@ reshape(a::Array, s::Size{S}) where {S} = s(a)
 @inline vec(a::StaticArray) = reshape(a, Size(prod(Size(typeof(a)))))
 
 @inline copy(a::StaticArray) = typeof(a)(Tuple(a))
+@inline copy(a::SizedArray) = typeof(a)(a)
 
 # TODO permutedims?
 
