3b1c73f115c535321e739c75ed2ec6e878680d4e
Chris Foster <chris42f@gmail.com>
2019-05-28 16:31:25 +1000

    Fix constructor for SVector{0} from Array (#608)
    Fixes #520

--- test/convert.jl
+++ test/convert.jl
@@ -5,3 +5,9 @@ using StaticArrays, Test
     SizeM = Size(2,2)(M)
     @test typeof(SizeM)(SizeM).data === M
 end # testset
+
+@testset "Constructors of zero size arrays" begin
+    # Issue #520
+    @testinf SVector{0}(Int8[]) === SVector{0,Int8}()
+    @testinf SMatrix{0,0}(zeros(0,0)) === SMatrix{0,0,Float64}(())
+end
