b53de4a5bfb96184b25cc37cffd8fd84e5c2f67c
Mateusz Baran <mateuszbaran89@gmail.com>
2019-07-02 09:24:22 +0200

    size_to_tuple changed to @pure

--- src/util.jl
+++ src/util.jl
@@ -43,7 +43,7 @@ end
 
 Converts a size given by `Tuple{N, M, ...}` into a tuple `(N, M, ...)`.
 """
-@generated function size_to_tuple(::Type{S}) where S<:Tuple
+@pure function size_to_tuple(::Type{S}) where S<:Tuple
     return tuple(S.parameters...)
 end
 
