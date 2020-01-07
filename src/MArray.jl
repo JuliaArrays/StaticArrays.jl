98db703d0ed74b0f062f350f7de7fdcd845f4989
Chris Foster <chris42f@gmail.com>
2020-01-07 16:02:25 +1000

    Document N type parameter of SArray and MArray (#712)

--- src/MArray.jl
+++ src/MArray.jl
@@ -1,15 +1,15 @@
 """
-    MArray{S, T, L}(undef)
-    MArray{S, T, L}(x::NTuple{L, T})
-    MArray{S, T, L}(x1, x2, x3, ...)
+    MArray{S, T, N, L}(undef)
+    MArray{S, T, N, L}(x::NTuple{L})
+    MArray{S, T, N, L}(x1, x2, x3, ...)
 
 
 Construct a statically-sized, mutable array `MArray`. The data may optionally be
 provided upon construction and can be mutated later. The `S` parameter is a Tuple-type
 specifying the dimensions, or size, of the array - such as `Tuple{3,4,5}` for a 3×4×5-sized
-array. The `L` parameter is the `length` of the array and is always equal to `prod(S)`.
-Constructors may drop the `L` and `T` parameters if they are inferrable from the input
-(e.g. `L` is always inferrable from `S`).
+array. The `N` parameter is the dimension of the array; the `L` parameter is the `length`
+of the array and is always equal to `prod(S)`. Constructors may drop the `L`, `N` and `T`
+parameters if they are inferrable from the input (e.g. `L` is always inferrable from `S`).
 
     MArray{S}(a::Array)
 
--- src/SArray.jl
+++ src/SArray.jl
@@ -1,13 +1,13 @@
 """
-    SArray{S, T, L}(x::NTuple{L, T})
-    SArray{S, T, L}(x1, x2, x3, ...)
+    SArray{S, T, N, L}(x::NTuple{L})
+    SArray{S, T, N, L}(x1, x2, x3, ...)
 
 Construct a statically-sized array `SArray`. Since this type is immutable, the data must be
 provided upon construction and cannot be mutated later. The `S` parameter is a Tuple-type
 specifying the dimensions, or size, of the array - such as `Tuple{3,4,5}` for a 3×4×5-sized
-array. The `L` parameter is the `length` of the array and is always equal to `prod(S)`.
-Constructors may drop the `L` and `T` parameters if they are inferrable from the input
-(e.g. `L` is always inferrable from `S`).
+array. The `N` parameter is the dimension of the array; the `L` parameter is the `length`
+of the array and is always equal to `prod(S)`. Constructors may drop the `L`, `N` and `T`
+parameters if they are inferrable from the input (e.g. `L` is always inferrable from `S`).
 
     SArray{S}(a::Array)
 
