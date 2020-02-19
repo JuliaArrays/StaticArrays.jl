403ea5a5611c9a685688b976a2dce26d9a7d2212
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-18 17:23:30 -0800

    [ci skip] Slightly more robust benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,7 +2,7 @@ using BenchmarkTools
 SUITE = BenchmarkGroup()
 for file in sort(readdir(@__DIR__))
     if startswith(file, "bench_") && endswith(file, ".jl")
-        SUITE[file[length("bench_") + 1:end - length(".jl")]] =
+        SUITE[chop(file, head = length("bench_"), tail = length(".jl"))] =
             include(file)
     end
 end
