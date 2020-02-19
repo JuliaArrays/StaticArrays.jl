bd834ff51d41ea1ee5e99f1b3721ddd4a06d189c
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-18 17:31:24 -0800

    [ci skip] Note on how files in `benchmark/` are organized

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,3 +1,7 @@
+# Each file of the form "bench_$(name).jl" in this directory is `include`d and
+# its last statement is assumed to be a `BenchmarkGroup`.  This group is added
+# to the top-level group `SUITE` with the `$name` extracted from the file name.
+
 using BenchmarkTools
 SUITE = BenchmarkGroup()
 for file in sort(readdir(@__DIR__))
