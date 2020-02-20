298f375dd09970e197bda26840a4f18535b04be9
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-19 18:50:04 -0800

    [ci skip] Reduce number of benchmarks to run in CI

--- benchmark/bench_matrix_ops.jl
+++ benchmark/bench_matrix_ops.jl
@@ -6,7 +6,11 @@ using LinearAlgebra
 using StaticArrays
 
 const suite = BenchmarkGroup()
-const Nmax = 20
+const matrix_sizes = if haskey(ENV, "GITHUB_EVENT_PATH")
+    (1, 2, 3, 4, 10, 20)
+else
+    1:20
+end
 
 # Use same arrays across processes (at least with the same Julia version):
 Random.seed!(1234)
@@ -14,7 +18,7 @@ Random.seed!(1234)
 # Unary operators
 for f in [det, inv, exp]
     s1 = suite["$f"] = BenchmarkGroup()
-    for N in 1:Nmax
+    for N in matrix_sizes
         SA = @SMatrix rand(N, N)
         A = Array(SA)
         s2 = s1["$N"] = BenchmarkGroup()
@@ -26,7 +30,7 @@ end
 # Binary operators
 for f in [*, \]
     s1 = suite["$f"] = BenchmarkGroup()
-    for N in 1:Nmax
+    for N in matrix_sizes
         SA = @SMatrix rand(N, N)
         SB = @SMatrix rand(N, N)
         A = Array(SA)
