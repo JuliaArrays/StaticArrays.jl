90cea142ca8d380e76193f0485b31bb7f20d021e
Steve Kelly <kd2cca@gmail.com>
2020-05-14 11:50:55 -0400

    [test] set version specific bounds in detect_ambiguities

--- test/ambiguities.jl
+++ test/ambiguities.jl
@@ -1,4 +1,13 @@
 
 # Allow no new ambiguities (see #18), unless you fix some old ones first!
-@test length(detect_ambiguities(Base, LinearAlgebra, StaticArrays)) <= 5
 
+const allowable_ambiguities =
+    if VERSION < v"1.1"
+        4
+    elseif VERSION < v"1.2"
+        2
+    else
+        1
+    end
+
+@test length(detect_ambiguities(Base, LinearAlgebra, StaticArrays)) <= allowable_ambiguities
