b1b3cb317e0c6f8bb10be0bea4a39edda3bfd196
hyrodium <hyrodium@gmail.com>
2021-01-17 23:26:18 +0900

    add more infomations to make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,19 @@
-using Documenter, StaticArrays
+using Documenter
+using StaticArrays
 
-makedocs(
-         modules = [StaticArrays],
-         sitename = "StaticArrays.jl",
-         pages = [
-            "Home" => "index.md",
-            "API" => "pages/api.md",
-            "Quick Start" => "pages/quickstart.md",
-            ],
-        )
-
-deploydocs(
-    repo = "github.com/JuliaArrays/StaticArrays.jl.git",
+makedocs(;
+    modules = [StaticArrays],
+    format = Documenter.HTML(
+        canonical = "https://JuliaArrays.github.io/StaticArrays.jl/stable/",
+        assets = ["assets/favicon.ico"],
+    ),
+    pages = [
+        "Home" => "index.md",
+        "API" => "pages/api.md",
+        "Quick Start" => "pages/quickstart.md",
+        ],
+    repo = "https://github.com/JuliaArrays/StaticArrays.jl/blob/{commit}{path}#L{line}",
+    sitename = "StaticArrays.jl",
 )
+
+deploydocs(; repo = "github.com/JuliaArrays/StaticArrays.jl")
