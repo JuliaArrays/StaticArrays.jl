dc7e64546430ffcb4643ba78b15f332c376cbd56
hyrodium <hyrodium@gmail.com>
2021-01-21 18:18:08 +0900

    update for pinv

--- src/StaticArrays.jl
+++ src/StaticArrays.jl
@@ -16,7 +16,7 @@ using Core.Compiler: return_type
 import Base: sqrt, exp, log, float, real
 using LinearAlgebra
 import LinearAlgebra: transpose, adjoint, dot, eigvals, eigen, lyap, tr,
-                      kron, diag, norm, dot, diagm, lu, svd, svdvals, pinv
+                      kron, diag, norm, dot, diagm, lu, svd, svdvals, pinv,
                       factorize, ishermitian, issymmetric, isposdef, issuccess, normalize,
                       normalize!, Eigen, det, logdet, logabsdet, cross, diff, qr, \
 using LinearAlgebra: checksquare
@@ -131,7 +131,6 @@ include("matrix_multiply.jl")
 include("lu.jl")
 include("det.jl")
 include("inv.jl")
-include("pinv.jl")
 include("solve.jl")
 include("eigen.jl")
 include("expm.jl")
@@ -144,6 +143,7 @@ include("qr.jl")
 include("deque.jl")
 include("flatten.jl")
 include("io.jl")
+include("pinv.jl")
 
 include("precompile.jl")
 _precompile_()
