cc545103f0163cf8fc0585b816d64903b4430110
Mateusz Baran <mateuszbaran89@gmail.com>
2021-07-07 11:56:13 +0200

    more reshape changes

--- test/abstractarray.jl
+++ test/abstractarray.jl
@@ -109,7 +109,7 @@ using StaticArrays, Test, LinearAlgebra
         @test @inferred(reshape(SVector(1,2,3,4), axes(SMatrix{2,2}(1,2,3,4)))) === SMatrix{2,2}(1,2,3,4)
         @test @inferred(reshape(SVector(1,2,3,4), Size(2,2))) === SMatrix{2,2}(1,2,3,4)
         @test @inferred(reshape([1,2,3,4], Size(2,2)))::SizedArray{Tuple{2,2},Int,2,1} == [1 3; 2 4]
-        @test_throws DimensionMismatch reshape([1 2; 3 4], Size(2,1,2))
+        @test_throws DimensionMismatch reshape([1 2; 3 4], Size(2,2,2))
 
         @test @inferred(vec(SMatrix{2, 2}([1 2; 3 4])))::SVector{4,Int} == [1, 3, 2, 4]
 
@@ -124,6 +124,10 @@ using StaticArrays, Test, LinearAlgebra
         m = @MMatrix [1 2; 3 4]
         mr = reshape(m, SOneTo(4))
         mr[2] = 10
+        @test m == SA[1 2; 10 4]
+
+        mrs = reshape(m, Size(4))
+        mrs[2] = 10
         @test m == SA[1 2; 10 4]
 
         ms = SizedMatrix{2,2}([1 2; 3 4])
