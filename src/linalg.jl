5cb4bdcd6720d0abcf8a00baf23f48d2e0a7661b
Wonseok Shin <wsshin97@gmail.com>
2022-01-08 23:53:35 -0500

    Avoid using iszero(scale) for speedup

--- src/linalg.jl
+++ src/linalg.jl
@@ -245,7 +245,7 @@ end
         $(Expr(:meta, :inline))
         scale = maxabs_nested(a)
 
-        iszero(scale) && return _init_zero(a)
+        scale==0 && return _init_zero(a)
         return @inbounds scale * sqrt($expr)
     end
 end
@@ -289,7 +289,7 @@ end
         $(Expr(:meta, :inline))
         scale = maxabs_nested(a)
 
-        iszero(scale) && return _init_zero(a)
+        scale==0 && return _init_zero(a)
         p == 1 && return @inbounds scale * $expr_p1
         return @inbounds scale * ($expr)^(inv(p))
     end
