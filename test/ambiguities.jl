3293270ac560e2e5b72098f85d5af15399296e76
Thomas Christensen <tchr@mit.edu>
2022-01-18 12:13:09 -0500

    resolve code-review by @c42f

--- test/ambiguities.jl
+++ test/ambiguities.jl
@@ -1,8 +1,8 @@
 # Allow no new ambiguities (see #18), unless you fix some old ones first!
 
-const allowable_ambiguities = 0
+const allowable_ambiguities = 55
 
 # TODO: Revisit and fix. See
 #   https://github.com/JuliaLang/julia/pull/36962
 #   https://github.com/JuliaLang/julia/issues/36951
-@test_broken length(detect_ambiguities(#=LinearAlgebra, =#StaticArrays)) <= allowable_ambiguities
+@test length(detect_ambiguities(#=LinearAlgebra, =#StaticArrays)) <= allowable_ambiguities
