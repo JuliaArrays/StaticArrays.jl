1d11c75f53325d7a8ed2197d3301b4a3c199de43
Thomas Christensen <tchr@mit.edu>
2022-01-21 15:54:54 -0500

    fix ambiguity counts
    - require unchanged ambiguity counts or an updated value of `allowable_ambiguities`

--- test/ambiguities.jl
+++ test/ambiguities.jl
@@ -1,8 +1,11 @@
 # Allow no new ambiguities (see #18), unless you fix some old ones first!
 
-const allowable_ambiguities = 55
+const allowable_ambiguities = VERSION ≥ v"1.7" ? 60 :
+                              VERSION ≥ v"1.6" ? 61 : error("version must be ≥1.6")
 
 # TODO: Revisit and fix. See
 #   https://github.com/JuliaLang/julia/pull/36962
 #   https://github.com/JuliaLang/julia/issues/36951
-@test length(detect_ambiguities(#=LinearAlgebra, =#StaticArrays)) <= allowable_ambiguities
+# Let's not allow new ambiguities. If any change makes the ambiguity count decrease, the
+# change should decrement `allowable_ambiguities` accordingly
+@test length(detect_ambiguities(#=LinearAlgebra, =#StaticArrays)) == allowable_ambiguities
