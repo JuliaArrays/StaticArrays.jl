07ee598d94a6bef72b9a0360886f789c2d4b0e70
nhz2 <nhz2@cornell.edu>
2022-06-07 14:52:20 -0400

    docs: fix docs for MMatrix and SMatrix macro

--- src/MMatrix.jl
+++ src/MMatrix.jl
@@ -36,7 +36,7 @@ end
     @MMatrix [a b c d]
     @MMatrix [[a, b];[c, d]]
     @MMatrix [i+j for i in 1:2, j in 1:2]
-    @MMatrix ones(2, 2, 2)
+    @MMatrix ones(2, 2)
 
 A convenience macro to construct `MMatrix`.
 See [`@SArray`](@ref) for detailed features.
