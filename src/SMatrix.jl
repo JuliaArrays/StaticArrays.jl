f395f356bc2ee0fb69d2187af084b3414961ad8f
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2022-06-07 17:33:10 -0400

    docs: fix docs for MMatrix and SMatrix macro (#1040)

--- src/SMatrix.jl
+++ src/SMatrix.jl
@@ -111,7 +111,7 @@ end
     @SMatrix [a b c d]
     @SMatrix [[a, b];[c, d]]
     @SMatrix [i+j for i in 1:2, j in 1:2]
-    @SMatrix ones(2, 2, 2)
+    @SMatrix ones(2, 2)
 
 A convenience macro to construct `SMatrix`.
 See [`@SArray`](@ref) for detailed features.
