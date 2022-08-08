ee3ee8eb617b923109f76a0c58f76b964e6f30f7
hyrodium <hyrodium@gmail.com>
2022-08-08 16:33:50 +0900

    add support for non-isbits type in _solve

--- src/solve.jl
+++ src/solve.jl
@@ -31,18 +31,25 @@ for Sa in [(2,2), (3,3)]  # not needed for Sa = (1, 1);
         @inline function _solve(::Size{$Sa}, ::Size{Sb}, a::StaticMatrix{<:Any, <:Any, Ta}, b::StaticMatrix{<:Any, <:Any, Tb}) where {Sb, Ta, Tb}
             d = det(a)
             T = typeof((one(Ta)*zero(Tb) + one(Ta)*zero(Tb))/d)
-            c = similar(b, T)
-            for col = 1:Sb[2]
-                @inbounds c[:, col] = _solve(Size($Sa), Size($Sa[1],), a, b[:, col])
+            if isbitstype(T)
+                # This if block can be removed when https://github.com/JuliaArrays/StaticArrays.jl/pull/749 is merged.
+                c = similar(b, T)
+                for col in 1:Sb[2]
+                    @inbounds c[:, col] = _solve(Size($Sa), Size($Sa[1],), a, b[:, col])
+                end
+                return similar_type(b, T)(c)
+            else
+                return _solve_general($(Size(Sa)), Size(Sb), a, b)
             end
-            return similar_type(b, T)(c)
         end
     end # @eval
 end
 
+@inline function _solve(sa::Size, sb::Size, a::StaticMatrix, b::StaticVecOrMat)
+    _solve_general(sa, sb, a, b)
+end
 
-
-@generated function _solve(::Size{Sa}, ::Size{Sb}, a::StaticMatrix{<:Any, <:Any, Ta}, b::StaticVecOrMat{Tb}) where {Sa, Sb, Ta, Tb}
+@generated function _solve_general(::Size{Sa}, ::Size{Sb}, a::StaticMatrix{<:Any, <:Any, Ta}, b::StaticVecOrMat{Tb}) where {Sa, Sb, Ta, Tb}
     if Sa[1] != Sb[1]
         return quote
             throw(DimensionMismatch("Left and right hand side first dimensions do not match in backdivide (got sizes $Sa and $Sb)"))
