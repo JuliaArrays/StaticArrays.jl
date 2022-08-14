f9c10d2f25213d1376678a11230ef71274cd0281
Yuto Horikawa <hyrodium@gmail.com>
2022-08-14 19:26:01 +0900

    Fix `_solve` with nonisbits type (#1071)
    * fix indents
    
    * add BigFloat in solve test
    
    * add support for non-isbits type in _solve
    
    * bump version to v1.5.3

--- src/solve.jl
+++ src/solve.jl
@@ -16,14 +16,14 @@ end
     T = typeof((one(Ta)*zero(Tb) + one(Ta)*zero(Tb))/d)
     @inbounds return similar_type(b, T)(
         ((a[2,2]*a[3,3] - a[2,3]*a[3,2])*b[1] +
-            (a[1,3]*a[3,2] - a[1,2]*a[3,3])*b[2] +
-            (a[1,2]*a[2,3] - a[1,3]*a[2,2])*b[3]) / d,
+         (a[1,3]*a[3,2] - a[1,2]*a[3,3])*b[2] +
+         (a[1,2]*a[2,3] - a[1,3]*a[2,2])*b[3]) / d,
         ((a[2,3]*a[3,1] - a[2,1]*a[3,3])*b[1] +
-            (a[1,1]*a[3,3] - a[1,3]*a[3,1])*b[2] +
-            (a[1,3]*a[2,1] - a[1,1]*a[2,3])*b[3]) / d,
+         (a[1,1]*a[3,3] - a[1,3]*a[3,1])*b[2] +
+         (a[1,3]*a[2,1] - a[1,1]*a[2,3])*b[3]) / d,
         ((a[2,1]*a[3,2] - a[2,2]*a[3,1])*b[1] +
-            (a[1,2]*a[3,1] - a[1,1]*a[3,2])*b[2] +
-            (a[1,1]*a[2,2] - a[1,2]*a[2,1])*b[3]) / d )
+         (a[1,2]*a[3,1] - a[1,1]*a[3,2])*b[2] +
+         (a[1,1]*a[2,2] - a[1,2]*a[2,1])*b[3]) / d )
 end
 
 for Sa in [(2,2), (3,3)]  # not needed for Sa = (1, 1);
@@ -31,18 +31,25 @@ for Sa in [(2,2), (3,3)]  # not needed for Sa = (1, 1);
         @inline function _solve(::Size{$Sa}, ::Size{Sb}, a::StaticMatrix{<:Any, <:Any, Ta}, b::StaticMatrix{<:Any, <:Any, Tb}) where {Sb, Ta, Tb}
             d = det(a)
             T = typeof((one(Ta)*zero(Tb) + one(Ta)*zero(Tb))/d)
-            c = similar(b, T)
-            for col = 1:Sb[2]
-                @inbounds c[:, col] = _solve(Size($Sa), Size($Sa[1],), a, b[:, col])
+            if isbitstype(T)
+                # This if block can be removed when https://github.com/JuliaArrays/StaticArrays.jl/pull/749 is merged.
+                c = similar(b, T)
+                for col in 1:Sb[2]
+                    @inbounds c[:, col] = _solve(Size($Sa), Size($Sa[1],), a, b[:, col])
+                end
+                return similar_type(b, T)(c)
+            else
+                return _solve_general($(Size(Sa)), Size(Sb), a, b)
             end
-            return similar_type(b, T)(c)
         end
     end # @eval
 end
 
+@inline function _solve(sa::Size, sb::Size, a::StaticMatrix, b::StaticVecOrMat)
+    _solve_general(sa, sb, a, b)
+end
 
-
-@generated function _solve(::Size{Sa}, ::Size{Sb}, a::StaticMatrix{<:Any, <:Any, Ta}, b::StaticVecOrMat{Tb}) where {Sa, Sb, Ta, Tb}
+@generated function _solve_general(::Size{Sa}, ::Size{Sb}, a::StaticMatrix{<:Any, <:Any, Ta}, b::StaticVecOrMat{Tb}) where {Sa, Sb, Ta, Tb}
     if Sa[1] != Sb[1]
         return quote
             throw(DimensionMismatch("Left and right hand side first dimensions do not match in backdivide (got sizes $Sa and $Sb)"))
