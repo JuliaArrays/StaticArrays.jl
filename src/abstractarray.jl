159befb43caae9728844f2e239b20977e662314b
N5N3 <2642243996@qq.com>
2022-08-24 16:57:32 +0800

    Define `similar_type` and `StaticArrayStyle` in the core package. (#1084)
    * Define `similar_type` and `StaticArrayStyle` in core package.
    
    See https://github.com/JuliaArrays/StaticArraysCore.jl/pull/5
    and https://github.com/JuliaArrays/StaticArraysCore.jl/pull/8
    
    * Bump version
    
    * Update Project.toml
    
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>
    
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>

--- src/abstractarray.jl
+++ src/abstractarray.jl
@@ -36,28 +36,6 @@ end
 Base.IndexStyle(::Type{T}) where {T<:StaticArray} = IndexLinear()
 
 # Default type search for similar_type
-"""
-    similar_type(static_array)
-    similar_type(static_array, T)
-    similar_type(array, ::Size)
-    similar_type(array, T, ::Size)
-
-Returns a constructor for a statically-sized array similar to the input array
-(or type) `static_array`/`array`, optionally with different element type `T` or size
-`Size`. If the input `array` is not a `StaticArray` then the `Size` is mandatory.
-
-This differs from `similar()` in that the resulting array type may not be
-mutable (or define `setindex!()`), and therefore the returned type may need to
-be *constructed* with its data.
-
-Note that the (optional) size *must* be specified as a static `Size` object (so the compiler
-can infer the result statically).
-
-New types should define the signature `similar_type(::Type{A},::Type{T},::Size{S}) where {A<:MyType,T,S}`
-if they wish to overload the default behavior.
-"""
-function similar_type end
-
 similar_type(::SA) where {SA<:StaticArrayLike} = similar_type(SA,eltype(SA))
 similar_type(::Type{SA}) where {SA<:StaticArrayLike} = similar_type(SA,eltype(SA))
 
