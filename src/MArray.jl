97b1c08561ca82699127d1e50761f4ed25d9b063
MasonProtter <mason.protter@icloud.com>
2023-04-02 21:52:38 -0600

    add in a `convert` for good measure

--- src/MArray.jl
+++ src/MArray.jl
@@ -27,7 +27,7 @@ end
     getfield(v,:data)[i]
 end
 
-@propagate_inbounds function setindex!(v::MArray, val, i::Int)
+@propagate_inbounds function setindex!(v::MArray, val, i::Int) 
     @boundscheck checkbounds(v,i)
     T = eltype(v)
 
@@ -37,7 +37,7 @@ end
         # This one is unsafe (#27)
         # unsafe_store!(Base.unsafe_convert(Ptr{Ptr{Nothing}}, pointer_from_objref(v.data)), pointer_from_objref(val), i)
         # error("setindex!() with non-isbitstype eltype is not supported by StaticArrays. Consider using SizedArray.")
-        setfield!(v, :data, Base.setindex(getfield(v, :data), val, i))
+        setfield!(v, :data, Base.setindex(getfield(v, :data), convert(eltype(v), val), i))
     end
     return v
 end
