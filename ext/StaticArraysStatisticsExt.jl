7ade82dc4d4ba0ba21072f2036fc86bfe36bf23d
Daniel Karrasch <daniel.karrasch@posteo.de>
2023-06-30 20:02:05 +0200

    Apply suggestions from code review
    Co-authored-by: Thomas Christensen <tchr@mit.edu>

--- ext/StaticArraysStatisticsExt.jl
+++ ext/StaticArraysStatisticsExt.jl
@@ -8,7 +8,6 @@ using StaticArrays: _InitialValue, _reduce, _mapreduce
 _mean_denom(a, ::Colon) = length(a)
 _mean_denom(a, dims::Int) = size(a, dims)
 _mean_denom(a, ::Val{D}) where {D} = size(a, D)
-_mean_denom(a, ::Type{Val{D}}) where {D} = size(a, D)
 
 @inline mean(a::StaticArray; dims=:) = _reduce(+, a, dims) / _mean_denom(a, dims)
 @inline mean(f::Function, a::StaticArray; dims=:) = _mapreduce(f, +, dims, _InitialValue(), Size(a), a) / _mean_denom(a, dims)
