0403edff1720186a949378a7804de6f1db6d8fae
hyrodium <hyrodium@gmail.com>
2023-12-02 08:48:46 +0900

    update tests on `@MVector` macro

--- test/MVector.jl
+++ test/MVector.jl
@@ -23,6 +23,20 @@
         # test for #557-like issues
         @test (@inferred MVector(SVector{0,Float64}()))::MVector{0,Float64} == MVector{0,Float64}()
 
+        @test MVector{3}(i for i in 1:3).data === (1,2,3)
+        @test MVector{3}(float(i) for i in 1:3).data === (1.0,2.0,3.0)
+        @test MVector{0,Int}().data === ()
+        @test MVector{3,Int}(i for i in 1:3).data === (1,2,3)
+        @test MVector{3,Float64}(i for i in 1:3).data === (1.0,2.0,3.0)
+        @test MVector{1}(MVector(MVector(1.0), MVector(2.0))[j] for j in 1:1) == MVector((MVector(1.0),))
+        @test_throws Exception MVector{3}(i for i in 1:2)
+        @test_throws Exception MVector{3}(i for i in 1:4)
+        @test_throws Exception MVector{3,Int}(i for i in 1:2)
+        @test_throws Exception MVector{3,Int}(i for i in 1:4)
+
+        @test MVector(1).data === (1,)
+        @test MVector(1,1.0).data === (1.0,1.0)
+
         @test ((@MVector [1.0])::MVector{1}).data === (1.0,)
         @test ((@MVector [1, 2, 3])::MVector{3}).data === (1, 2, 3)
         @test ((@MVector Float64[1,2,3])::MVector{3}).data === (1.0, 2.0, 3.0)
@@ -31,25 +45,64 @@
 
         @test ((@MVector zeros(2))::MVector{2, Float64}).data === (0.0, 0.0)
         @test ((@MVector ones(2))::MVector{2, Float64}).data === (1.0, 1.0)
-        @test ((@MVector fill(2.5, 2))::MVector{2, Float64}).data === (2.5, 2.5)
-        @test isa(@MVector(rand(2)), MVector{2, Float64})
-        @test isa(@MVector(randn(2)), MVector{2, Float64})
-        @test isa(@MVector(randexp(2)), MVector{2, Float64})
-
+        @test ((@MVector fill(2.5, 2))::MVector{2,Float64}).data === (2.5, 2.5)
         @test ((@MVector zeros(Float32, 2))::MVector{2,Float32}).data === (0.0f0, 0.0f0)
         @test ((@MVector ones(Float32, 2))::MVector{2,Float32}).data === (1.0f0, 1.0f0)
-        @test isa(@MVector(rand(Float32, 2)), MVector{2, Float32})
-        @test isa(@MVector(randn(Float32, 2)), MVector{2, Float32})
-        @test isa(@MVector(randexp(Float32, 2)), MVector{2, Float32})
 
-        test_expand_error(:(@MVector fill(1.5, 2, 3)))
-        test_expand_error(:(@MVector ones(2, 3, 4)))
-        test_expand_error(:(@MVector sin(1:5)))
-        test_expand_error(:(@MVector [i*j for i in 1:2, j in 2:3]))
-        test_expand_error(:(@MVector Float32[i*j for i in 1:2, j in 2:3]))
-        test_expand_error(:(@MVector [1; 2; 3]...))
-        test_expand_error(:(@MVector a))
-        test_expand_error(:(@MVector [[1 2];[3 4]]))
+        @testset "@MVector rand*" begin
+            n = 4
+            @test (@MVector rand(n)) isa MVector{n, Float64}
+            @test (@MVector randn(n)) isa MVector{n, Float64}
+            @test (@MVector randexp(n)) isa MVector{n, Float64}
+            @test (@MVector rand(4)) isa MVector{4, Float64}
+            @test (@MVector randn(4)) isa MVector{4, Float64}
+            @test (@MVector randexp(4)) isa MVector{4, Float64}
+            @test (@MVector rand(_rng(), n)) isa MVector{n, Float64}
+            @test (@MVector rand(_rng(), n)) == rand(_rng(), n)
+            @test (@MVector randn(_rng(), n)) isa MVector{n, Float64}
+            @test (@MVector randn(_rng(), n)) == randn(_rng(), n)
+            @test (@MVector randexp(_rng(), n)) isa MVector{n, Float64}
+            @test (@MVector randexp(_rng(), n)) == randexp(_rng(), n)
+            @test (@MVector rand(_rng(), 4)) isa MVector{4, Float64}
+            @test (@MVector rand(_rng(), 4)) == rand(_rng(), 4)
+            @test (@MVector randn(_rng(), 4)) isa MVector{4, Float64}
+            @test (@MVector randn(_rng(), 4)) == randn(_rng(), 4)
+            @test (@MVector randexp(_rng(), 4)) isa MVector{4, Float64}
+            @test (@MVector randexp(_rng(), 4)) == randexp(_rng(), 4)
+
+            for T in (Float32, Float64)
+                @test (@MVector rand(T, n)) isa MVector{n, T}
+                @test (@MVector randn(T, n)) isa MVector{n, T}
+                @test (@MVector randexp(T, n)) isa MVector{n, T}
+                @test (@MVector rand(T, 4)) isa MVector{4, T}
+                @test (@MVector randn(T, 4)) isa MVector{4, T}
+                @test (@MVector randexp(T, 4)) isa MVector{4, T}
+                @test (@MVector rand(_rng(), T, n)) isa MVector{n, T}
+                # @test (@MVector rand(_rng(), T, n)) == rand(_rng(), T, n)
+                @test (@MVector randn(_rng(), T, n)) isa MVector{n, T}
+                @test (@MVector randn(_rng(), T, n)) == randn(_rng(), T, n)
+                @test (@MVector randexp(_rng(), T, n)) isa MVector{n, T}
+                @test (@MVector randexp(_rng(), T, n)) == randexp(_rng(), T, n)
+                @test (@MVector rand(_rng(), T, 4)) isa MVector{4, T}
+                # @test (@MVector rand(_rng(), T, 4)) == rand(_rng(), T, 4)
+                @test (@MVector randn(_rng(), T, 4)) isa MVector{4, T}
+                @test (@MVector randn(_rng(), T, 4)) == randn(_rng(), T, 4)
+                @test (@MVector randexp(_rng(), T, 4)) isa MVector{4, T}
+                @test (@MVector randexp(_rng(), T, 4)) == randexp(_rng(), T, 4)
+            end
+        end
+
+        @testset "expand error" begin
+            test_expand_error(:(@MVector fill(1.5, 2, 3)))
+            test_expand_error(:(@MVector ones(2, 3, 4)))
+            test_expand_error(:(@MVector rand(Float64, 2, 3, 4)))
+            test_expand_error(:(@MVector sin(1:5)))
+            test_expand_error(:(@MVector [i*j for i in 1:2, j in 2:3]))
+            test_expand_error(:(@MVector Float32[i*j for i in 1:2, j in 2:3]))
+            test_expand_error(:(@MVector [1; 2; 3]...))
+            test_expand_error(:(@MVector a))
+            test_expand_error(:(@MVector [[1 2];[3 4]]))
+        end
 
         if VERSION >= v"1.7.0"
             @test ((@MVector Float64[1;2;3;;;])::MVector{3}).data === (1.0, 2.0, 3.0)
