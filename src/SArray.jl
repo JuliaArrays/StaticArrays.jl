8d7d302a6fb4c93d6e98533ffc7b7c8755070f05
Abhro R <5664668+abhro@users.noreply.github.com>
2024-06-29 16:16:19 -0400

    Fix markdown lists
    Add spacing before list items so that markdown treats them as one list

--- src/SArray.jl
+++ src/SArray.jl
@@ -307,19 +307,19 @@ end
 A convenience macro to construct `SArray` with arbitrary dimension.
 It supports:
 1. (typed) array literals.
-!!! note
-    Every argument inside the square brackets is treated as a scalar during expansion.
-    Thus `@SArray[a; b]` always forms a `SVector{2}` and `@SArray [a b; c]` always throws
-    an error.
+   !!! note
+       Every argument inside the square brackets is treated as a scalar during expansion.
+       Thus `@SArray[a; b]` always forms a `SVector{2}` and `@SArray [a b; c]` always throws
+       an error.
 
 2. comprehensions
-!!! note
-    The range of a comprehension is evaluated at global scope by the macro, and must be
-    made of combinations of literal values, functions, or global variables.
+   !!! note
+       The range of a comprehension is evaluated at global scope by the macro, and must be
+       made of combinations of literal values, functions, or global variables.
 
 3. initialization functions
-!!! note
-    Only support `zeros()`, `ones()`, `fill()`, `rand()`, `randn()`, and `randexp()`
+   !!! note
+       Only support `zeros()`, `ones()`, `fill()`, `rand()`, `randn()`, and `randexp()`
 """
 macro SArray(ex)
     static_array_gen(SArray, ex, __module__)
