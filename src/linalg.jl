fe768a7c1a5003ce6090f52ec7d985120611e508
Alexander Plavin <alexander@plav.in>
2026-03-09 11:21:18 -0400

    Fix zero() for LinearAlgebra wrappers over StaticArrays

--- src/linalg.jl
+++ src/linalg.jl
@@ -148,6 +148,15 @@ end
 @inline Base.zero(a::SA) where {SA <: StaticArray} = zeros(SA)
 @inline Base.zero(a::Type{SA}) where {SA <: StaticArray} = zeros(SA)
 
+# Wrappers over StaticArray (see StaticMatrixLike definition)
+for WR in (:Symmetric, :Hermitian)
+    @eval @inline Base.zero(a::$WR{<:Any,<:StaticMatrix}) = $WR(zero(parent(a)), _sym_uplo(a))
+end
+for WR in (:UpperTriangular, :LowerTriangular, :UnitUpperTriangular, :UnitLowerTriangular, :Transpose, :Adjoint)
+    @eval @inline Base.zero(a::$WR{<:Any,<:StaticArray}) = $WR(zero(parent(a)))
+end
+@inline Base.zero(a::Diagonal{<:Any,<:StaticVector}) = Diagonal(zero(parent(a)))
+
 @inline _construct_sametype(a::Type, elements) = a(elements)
 @inline _construct_sametype(a, elements) = typeof(a)(elements)
 
